9c7d120c1503add27f270051a71340edce4e2c26
SIbaprasad <sp.dobest@gmail.com>
2017-03-27 19:31:37 +0530

    dasd

--- app/src/main/java/sptecch/sp_facebookexample/MainActivity.java
+++ app/src/main/java/sptecch/sp_facebookexample/MainActivity.java
@@ -1,13 +1,36 @@
 package sptecch.sp_facebookexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
+    AppCompatButton loginButton;
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = (AppCompatButton) findViewById(R.id.buttonFacebookLOgin);
+
+
+
+    }
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
